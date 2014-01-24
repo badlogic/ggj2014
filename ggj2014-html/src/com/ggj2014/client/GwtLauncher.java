051981668a28cd096fc37affef4ae0b29391a598
badlogic <badlogicgames@gmail.com>
2014-01-24 22:18:39 +0100

    initial commit

---
+++ ggj2014-html/src/com/ggj2014/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.ggj2014.client;
+
+import com.ggj2014.Game;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Game();
+	}
+}
