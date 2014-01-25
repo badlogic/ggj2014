c80015ac7c063a4f96bcfe1093aafe220618f921
Xifiggam <xifiggam@gmx.at>
2014-01-25 13:31:56 +0100

    Editing Enemy.java

--- ggj2014/src/com/ggj2014/mechanic/Entity.java
+++ ggj2014/src/com/ggj2014/mechanic/Entity.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 public abstract class Entity {
 	public Vector2 position = new Vector2();
 	public boolean isVisible = true;
+	public float delta;
 	
 	public Entity(float x, float y) {
 		this.position.set(x, y);
