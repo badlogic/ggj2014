4202023ebdce95fa93c1b2bd7335cecda349a585
Jörg Müller <nexyon@gmail.com>
2014-01-25 13:15:12 +0100

    Player and Goal entities.

--- ggj2014/src/com/ggj2014/mechanic/Entity.java
+++ ggj2014/src/com/ggj2014/mechanic/Entity.java
@@ -3,10 +3,14 @@ package com.ggj2014.mechanic;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class Entity {
-	public Vector2 position = new Vector2();
+	public Vector2 position;
 	
 	public Entity(float x, float y) {
-		this.position.set(x, y);
+		this.position = new Vector2(x, y);
+	}
+	
+	public Entity(Vector2 position) {
+		this.position = position;
 	}
 	
 	public abstract void update(World world, float deltaTime);
