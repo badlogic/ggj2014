54ad0f11883515acd7b1d2da1d84cc6dcb2f8637
Xifiggam <xifiggam@gmx.at>
2014-01-25 14:11:45 +0100

    Some msg

---
+++ ggj2014/src/com/ggj2014/mechanic/Pill.java
@@ -0,0 +1,16 @@
+package com.ggj2014.mechanic;
+
+public class Pill extends Entity {
+
+	public Pill(float x, float y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(World world, float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
