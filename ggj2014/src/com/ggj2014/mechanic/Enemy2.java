c1965b7fb705f54ecdb41999be8761b29000667c
badlogic <badlogicgames@gmail.com>
2014-01-25 17:41:22 +0100

    derriving enemy2 from enemy

--- ggj2014/src/com/ggj2014/mechanic/Enemy2.java
+++ ggj2014/src/com/ggj2014/mechanic/Enemy2.java
@@ -1,23 +1,7 @@
 package com.ggj2014.mechanic;
  
-public class Enemy2 extends Entity {
-	public float combatRange = 1;
-	public float sightRange = 5;
-	public float speed = 2;
-	public float damage = 10;
-	public State state = State.IDLE;
-	public float stateDuration;
-	
-	
-	public Enemy2(float x, float y) {
+public class Enemy2 extends Enemy {
+	public Enemy2 (float x, float y) {
 		super(x, y);
-	}
-
-	@Override
-	public void update(World world, float deltaTime) {
-	}
-	
-	enum State {
-		IDLE, MOVING_LEFT, MOVING_RIGHT, ATTACKING, WANDERING 
 	}
 }
