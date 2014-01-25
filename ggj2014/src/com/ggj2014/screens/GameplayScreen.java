e7af915f54a87d797afa0d1a25b3d47873365294
badlogic <contact@badlogicgames.com>
2014-01-25 12:54:33 +0100

    gameplay screen skeleton

--- ggj2014/src/com/ggj2014/screens/GameplayScreen.java
+++ ggj2014/src/com/ggj2014/screens/GameplayScreen.java
@@ -1,5 +1,27 @@
 package com.ggj2014.screens;
 
-public class GameplayScreen {
+import com.badlogic.gdx.Gdx;
+import com.ggj2014.Screen;
+import com.ggj2014.ScreenManager;
+import com.ggj2014.mechanic.World;
+import com.ggj2014.mechanic.WorldRenderer;
 
+public class GameplayScreen extends Screen {
+	World world;
+	WorldRenderer renderer;
+	
+	public GameplayScreen (ScreenManager manager) {
+		super(manager);
+	}
+
+	@Override
+	public void render () {
+		float delta = Gdx.graphics.getDeltaTime();
+		world.update(delta);
+		renderer.render(world, delta);
+	}
+
+	@Override
+	public void dispose () {
+	}
 }
