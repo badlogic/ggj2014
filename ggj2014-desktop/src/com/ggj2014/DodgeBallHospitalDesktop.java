8f1015c07f9efa954bb97ec6daafbf69beaa81d8
Xifiggam <xifiggam@gmx.at>
2014-01-26 13:41:05 +0100

    lvl change

--- ggj2014-desktop/src/com/ggj2014/DodgeBallHospitalDesktop.java
+++ ggj2014-desktop/src/com/ggj2014/DodgeBallHospitalDesktop.java
@@ -11,6 +11,8 @@ public class DodgeBallHospitalDesktop {
 		cfg.width = 1024;
 		cfg.height = 768;
 		
+		//cfg.setFromDisplayMode(LwjglApplicationConfiguration.getDesktopDisplayMode());
+		
 		new LwjglApplication(new DodgeBallHospital(), cfg);
 	}
 }
