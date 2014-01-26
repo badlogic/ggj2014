9637b4f1ee303356c90182ba0e618fc62cc7af19
Xifiggam <xifiggam@gmx.at>
2014-01-26 13:02:19 +0100

    Screens

--- ggj2014/src/com/ggj2014/screens/WinScreen.java
+++ ggj2014/src/com/ggj2014/screens/WinScreen.java
@@ -2,21 +2,32 @@ package com.ggj2014.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ggj2014.Screen;
 import com.ggj2014.ScreenManager;
 
 public class WinScreen extends Screen {
 
+	Texture texture = new Texture(Gdx.files.internal("graphics/win-screen.png"));
+	SpriteBatch batch;
+	
 	public WinScreen(ScreenManager manager) {
 		super(manager);
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render() {
-		// TODO: render beautiful game over graphics :D
 
 		Gdx.gl20.glClearColor(0.4f, 0.4f, 1, 1);
 		Gdx.gl20.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+		
 		
 		if(Gdx.input.isKeyPressed(Keys.ESCAPE)) {
 			Gdx.app.exit();
@@ -25,6 +36,8 @@ public class WinScreen extends Screen {
 
 	@Override
 	public void dispose() {
+		batch.dispose();
+		texture.dispose();
 	}
 
 }
