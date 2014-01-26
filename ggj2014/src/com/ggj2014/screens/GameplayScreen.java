45f8549b72e87c3acd72f769b1c9d1fd6f90b0ab
badlogic <contact@badlogicgames.com>
2014-01-26 12:42:44 +0100

    audio

--- ggj2014/src/com/ggj2014/screens/GameplayScreen.java
+++ ggj2014/src/com/ggj2014/screens/GameplayScreen.java
@@ -2,6 +2,7 @@ package com.ggj2014.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.ggj2014.AudioManager;
 import com.ggj2014.DodgeBallHospital;
 import com.ggj2014.Screen;
 import com.ggj2014.ScreenManager;
@@ -12,12 +13,14 @@ import com.ggj2014.mechanic.WorldRenderer;
 public class GameplayScreen extends Screen {
 	World world;
 	WorldRenderer renderer;
+	AudioManager audio;
 	
 	public GameplayScreen (ScreenManager manager) {
 		super(manager);
 		
 		world = new World("levels/map1_v2.tmx");
 		renderer = new WorldRenderer(world);
+		audio = new AudioManager();
 		world.setRenderer(renderer);
 	}
 
@@ -41,6 +44,7 @@ public class GameplayScreen extends Screen {
 	@Override
 	public void dispose () {
 		renderer.dispose();
+		audio.dispose();
 		ScreenManager.multiplexer.clear();
 	}
 }
