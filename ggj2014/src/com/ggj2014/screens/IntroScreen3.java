c8c01eade7e01938c037819477166ce440046b72
Xifiggam <xifiggam@gmx.at>
2014-01-26 13:18:31 +0100

    Introscreens

---
+++ ggj2014/src/com/ggj2014/screens/IntroScreen3.java
@@ -0,0 +1,53 @@
+package com.ggj2014.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.ggj2014.Screen;
+import com.ggj2014.ScreenManager;
+
+public class IntroScreen3 extends Screen {
+
+	Texture texture = new Texture(Gdx.files.internal("graphics/intro-panel-1.png"));
+	SpriteBatch batch;
+	InputAdapter input;
+	
+	public IntroScreen3(final ScreenManager manager) {
+		super(manager);
+		batch = new SpriteBatch();
+		input = new InputAdapter(){
+
+			@Override
+			public boolean keyDown(int keycode) {
+				// TODO Auto-generated method stub
+				manager.setScreen(new GameplayScreen(manager));
+				return super.keyDown(keycode);
+			}
+			
+		};
+		manager.multiplexer.addProcessor(input);
+	}
+
+	@Override
+	public void render() {
+
+		Gdx.gl20.glClearColor(0.4f, 0.4f, 1, 1);
+		Gdx.gl20.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+		
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		texture.dispose();
+		manager.multiplexer.removeProcessor(input);
+	}
+
+}
