67f1be19af3f3eddb1ca13e13f35ca825485d920
badlogic <contact@badlogicgames.com>
2014-01-26 13:31:13 +0100

    fixed screens

--- ggj2014/src/com/ggj2014/screens/IntroScreenInstruction.java
+++ ggj2014/src/com/ggj2014/screens/IntroScreenInstruction.java
@@ -1,10 +1,8 @@
 package com.ggj2014.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ggj2014.Screen;
 import com.ggj2014.ScreenManager;
@@ -18,36 +16,25 @@ public class IntroScreenInstruction extends Screen {
 	public IntroScreenInstruction(final ScreenManager manager) {
 		super(manager);
 		batch = new SpriteBatch();
-		input = new InputAdapter(){
-
-			@Override
-			public boolean keyDown(int keycode) {
-				// TODO Auto-generated method stub
-				manager.setScreen(new IntroScreen1(manager));
-				return super.keyDown(keycode);
-			}
-			
-		};
-		manager.multiplexer.addProcessor(input);
 	}
 
 	@Override
 	public void render() {
-
 		Gdx.gl20.glClearColor(0.4f, 0.4f, 1, 1);
 		Gdx.gl20.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
         
-        batch.begin();
-        batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
+     batch.begin();
+     batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+     batch.end();
 		
+     if(Gdx.input.justTouched()) {
+   	  manager.setScreen(new IntroScreen1(manager));
+     }
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
 		texture.dispose();
-		manager.multiplexer.removeProcessor(input);
 	}
-
 }
